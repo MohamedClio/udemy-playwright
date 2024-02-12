5bc93359efd575fad025fde3b6ccd59cfba3ed50
@herrmclio <herrmclio@gmail.com>
2024-02-12 23:11:40 +0100

    adding searchbox test cases

--- helpers.ts
+++ helpers.ts
@@ -2,6 +2,10 @@ export async function loadMainPage(page) {
   await page.goto('https://www.example.com')
 }
 
+export async function bankingHomePage(page) {
+  await page.goto('http://zero.webappsecurity.com/index.html')
+}
+
 export async function assertTitle(page) {
   await page.waitForSelector('h1')
 }
